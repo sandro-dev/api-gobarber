a11adb5fb23ff1d177786d458ebaedb1ab74b0e0
Sandro Santos <sandro.dev@gmail.com>
2019-12-28 12:50:49 -0300

    method delete added on Appointments Controller

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import { pt } from 'date-fns/locale/pt-BR';
 
 import Appointment from '../models/Appointment';
@@ -104,6 +104,29 @@ class AppointmentController {
       content: `Novo agendamento de ${user.name} para o ${formattedDate}`,
       user: provider_id,
     });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment",
+      });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res.status(401).json({
+        error: 'You can only cancel this appointment 2 hours before',
+      });
+    }
+
+    appointment.canceled_at = new Date();
+    appointment.save();
 
     return res.json(appointment);
   }
